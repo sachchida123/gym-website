def06ff0c44eae15db0ab3dc0e0078f34162daeb
sachchida123 <112744705+sachchida123@users.noreply.github.com>
2023-04-14 11:09:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Hero from './components/hero/Hero';
+import Program from './components/Program';
+function App() {
+  return (
+    <div className="App">
+          <Hero/>
+          <Program/>
+    </div>
+  );
+}
+
+export default App;
